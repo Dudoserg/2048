096b0fbb916c7a4b6d66c85f141a41cc3d109a9a
Pankratov Sergey <frielender@gmail.com>
2019-03-16 17:33:50 +0700

    create one more test for game.up(); This test passed successes

--- src/Tests/TestClass.java
+++ src/Tests/TestClass.java
@@ -279,4 +279,24 @@ public class TestClass {
         Assertions.assertArrayEquals(result,game.getArr());
     }
 
+    @Test
+    void upTest2(){
+        Game game = new Game();
+        game.init();
+        game.setElem(0,0,4);game.setElem(0,1,0);game.setElem(0,2,0);game.setElem(0,3,0);
+        game.setElem(1,0,2);game.setElem(1,1,0);game.setElem(1,2,4);game.setElem(1,3,0);
+        game.setElem(2,0,0);game.setElem(2,1,0);game.setElem(2,2,0);game.setElem(2,3,0);
+        game.setElem(3,0,2);game.setElem(3,1,0);game.setElem(3,2,0);game.setElem(3,3,0);
+
+        game.up();
+
+        int result[][] = {
+                {4,0,4,0},
+                {4,0,0,0},
+                {0,0,0,0},
+                {0,0,0,0},
+        };
+
+        Assertions.assertArrayEquals(result,game.getArr());
+    }
 }
