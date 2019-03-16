1dd957642a14fc7a5f1822850b2d37f501b0dd12
Pankratov Sergey <frielender@gmail.com>
2019-03-16 17:34:16 +0700

    create JavaDoc for game.up()

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -146,6 +146,10 @@ public class Game {
             }
         }
     }
+
+    /**
+     * Сдвиг матрицы вверх
+     */
     public void up() {
         for(int i = 0 ; i < this.size; i++){
             for(int j = 0 ; j < this.size ; j++){
