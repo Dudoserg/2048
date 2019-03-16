ac1e3ab80f9319739bac0968ba5bb799b1f055cd
Pankratov Sergey <frielender@gmail.com>
2019-03-16 17:25:57 +0700

    create one more test for game.right(); This test passed successes

--- src/Tests/TestClass.java
+++ src/Tests/TestClass.java
@@ -161,6 +161,27 @@ public class TestClass {
     }
 
     @Test
+    void rightTest2(){
+        Game game = new Game();
+        game.init();
+        game.setElem(0,0,16);game.setElem(0,1,8);game.setElem(0,2,2);game.setElem(0,3,2);
+        game.setElem(1,0,2);game.setElem(1,1,0);game.setElem(1,2,0);game.setElem(1,3,0);
+        game.setElem(2,0,4);game.setElem(2,1,0);game.setElem(2,2,0);game.setElem(2,3,0);
+        game.setElem(3,0,2);game.setElem(3,1,0);game.setElem(3,2,0);game.setElem(3,3,2);
+
+        game.right();
+
+        int result[][] = {
+                {0,16,8,4},
+                {0,0,0,2},
+                {0,0,0,4},
+                {0,0,0,4},
+        };
+
+        Assertions.assertArrayEquals(result,game.getArr());
+    }
+
+    @Test
     void motionElemTest(){
         Game game = new Game();
         game.init();
