68454092f918d271afab04decf63f0ac48db77fe
Pankratov Sergey <frielender@gmail.com>
2019-03-16 13:49:36 +0700

    first commit

---
+++ src/Tests/TestClass.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    void gameClassCreationTest(){
+        Game game = new Game();
+        Assertions.assertNotNull(game);
+    }
+}
